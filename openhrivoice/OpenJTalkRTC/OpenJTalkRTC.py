e7d82ebd9421cb995f761798c93f9c4fc29e3f98
Isao Hara <isao-hara@aist.go.jp>
2017-08-29 14:13:00 +0900

    update openjtalkrtc

--- openhrivoice/OpenJTalkRTC/OpenJTalkRTC.py
+++ openhrivoice/OpenJTalkRTC/OpenJTalkRTC.py
@@ -157,7 +157,6 @@ class OpenJTalkWrap(VoiceSynthBase):
         # normally openjtalk outputs 48000Hz sound.
         wavfile2 = self.gettempname()
         cmdarg = [self._conf._sox_bin, "-t", "wav", wavfile, "-r", str(samplerate), "-t", "wav", wavfile2]
-        print cmdarg
         p = subprocess.Popen(cmdarg)
         p.wait()
 
